e796a89b960eba61069208a945c542f48c473c48
Filcap88 <filcap@tiscali.it>
2014-05-25 13:23:07 +0200

    finish hw3

--- 2014-05-16/python/exercise4.py
+++ 2014-05-16/python/exercise4.py
@@ -1,1 +1,41 @@
 # ex4
+
+from pyplasm import *
+from scipy import *
+import os,sys
+# inserire il path corretto per la libreria larcc
+sys.path.insert(0, 'C:/Python26/Lib/site-packages/lar-cc/lib/py/')
+from lar2psm import *
+from simplexn import *
+from larcc import *
+from largrid import *
+from mapper import *
+from boolean import *
+from sysml import *
+
+def diagram2cellMatrix(diagram):
+   def diagramToCellMatrix0(master,cell):
+      wdw = min(diagram[0]) + max(diagram[0])         # window3D
+      cV = [master[0][v] for v in master[1][cell]]
+      vpt = min(cV) + max(cV)                      # viewport3D
+      print "\n window3D =",wdw
+      print "\n viewport3D =",vpt
+      
+      mat = zeros((4,4))
+      mat[0,0] = (vpt[3]-vpt[0])/(wdw[3]-wdw[0])
+      mat[0,3] = vpt[0] - mat[0,0]*wdw[0]
+      mat[1,1] = (vpt[4]-vpt[1])/(wdw[4]-wdw[1])
+      mat[1,3] = vpt[1] - mat[1,1]*wdw[1]
+      mat[2,2] = (vpt[5]-vpt[2])/(wdw[5]-wdw[2])
+      mat[2,3] = vpt[2] - mat[2,2]*wdw[2]
+      mat[3,3] = 1
+      print "\n mat =",mat
+      return mat
+   return diagramToCellMatrix0
+
+def newdiagram2cell(diagram, master, cell):
+    mat = diagram2cellMatrix(diagram)(master,cell)
+    diagram = larApply(mat)(diagram)
+    V, CV1, CV2, lencase12 = vertexSieve(master, diagram)
+    master = V, CV1+CV2
+    return master;
